67a7ccf059fe948236ce1e5faaf5e21ce72b0e54
samuel <wiggerssamuel2005@gmail.com>
2025-11-12 19:34:10 -0300

    ok

--- TrabalhoFinal/config.php
+++ TrabalhoFinal/config.php
@@ -2,7 +2,7 @@
 
 /** Define as credenciais do banco de dados PostgreSQL */
 define('DB_HOST', 'localhost'); // Endereço do servidor PostgreSQL
-define('DB_NAME', 'TrabalhoFinal');  // Nome do banco de dados
+define('DB_NAME', 'avaliacao');  // Nome do banco de dados
 define('DB_USER', 'postgres');  // Usuário do banco
 define('DB_PASS', '123');       // Senha do usuário
 
